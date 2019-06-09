35aebeca1d2a1601c6a8bf228563e20b9692a8de
Jatyn Stacy <jlee0964@gmail.com>
2019-06-09 00:05:36 -0400

    Filter out empty (null) inventory slots while iterating over inventory contents

--- src/main/java/net/jingles/enchantments/util/InventoryUtils.java
+++ src/main/java/net/jingles/enchantments/util/InventoryUtils.java
@@ -4,12 +4,14 @@ import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.inventory.Inventory;
 
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class InventoryUtils {
 
   public static void removeItem(Inventory inventory, Material material, int amount) {
     Stream.of(inventory.getContents())
+        .filter(Objects::nonNull)
         .filter(item -> item.getType() == material)
         .findFirst().ifPresent(item -> {
           int newAmount = item.getAmount() - amount;
@@ -21,6 +23,7 @@ public class InventoryUtils {
 
   public static void removeItem(Inventory inventory, Tag<Material> tag, int amount) {
     Stream.of(inventory.getContents())
+        .filter(Objects::nonNull)
         .filter(item -> tag.isTagged(item.getType()))
         .findFirst().ifPresent(item -> {
           int newAmount = item.getAmount() - amount;
