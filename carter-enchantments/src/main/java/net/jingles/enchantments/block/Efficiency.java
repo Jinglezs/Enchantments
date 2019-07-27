688169e1c4a7d386b418c6ad89463464621327c0
Jatyn Stacy <jlee0964@gmail.com>
2019-07-26 23:09:16 -0400

    Add Enchant annotation to efficiency *facepalm*

--- carter-enchantments/src/main/java/net/jingles/enchantments/block/Efficiency.java
+++ carter-enchantments/src/main/java/net/jingles/enchantments/block/Efficiency.java
@@ -2,12 +2,20 @@ package net.jingles.enchantments.block;
 
 import net.jingles.enchantments.Enchantments;
 import net.jingles.enchantments.enchant.BlockEnchant;
+import net.jingles.enchantments.enchant.Enchant;
+import net.jingles.enchantments.enchant.TargetGroup;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Container;
 import org.bukkit.block.Furnace;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.FurnaceBurnEvent;
+
+@Enchant(name = "Efficiency", key = "efficiency", enchantChance = 0.45, levelRequirement = 20,
+    targetItem = EnchantmentTarget.ALL, targetGroup = TargetGroup.CONTAINER, maxLevel = 10,
+    description = "Increases how long fuel burns for by 10% per level and decreases the time " +
+        "it takes to smelt any item by 10% per level.")
 
 public class Efficiency extends BlockEnchant {
 
@@ -30,10 +38,10 @@ public class Efficiency extends BlockEnchant {
   public void onItemBurn(FurnaceBurnEvent event) {
 
     Furnace furnace = (Furnace) event.getBlock().getState();
-    int level = getLevel(furnace);
+    int level = getLevel(furnace) * 10;
 
     int burnTime = event.getBurnTime() + (event.getBurnTime() * (level / 100));
-    int cookTime = furnace.getCookTimeTotal() - (furnace.getCookTimeTotal() * (level/100));
+    int cookTime = furnace.getCookTimeTotal() - (furnace.getCookTimeTotal() * (level / 100));
 
     event.setBurnTime(burnTime);
     furnace.setCookTimeTotal(cookTime);
