d94cd39ff937032e6d51af263b41d815233c9112
Jatyn Stacy <jlee0964@gmail.com>
2019-07-29 13:08:23 -0400

    Added Fortuna's Blessing (banner enchantment).
    This enchantment increases the likelihood of an entity dropping its equipment by 50% and has a 45% chance of doubling the amount of dropped items and experience when a mob is killed within the same chunk as the enchanted banner.

---
+++ carter-enchantments/src/main/java/net/jingles/enchantments/block/FortunasBlessing.java
@@ -0,0 +1,85 @@
+package net.jingles.enchantments.block;
+
+import net.jingles.enchantments.enchant.BlockEnchant;
+import net.jingles.enchantments.enchant.Enchant;
+import net.jingles.enchantments.enchant.TargetGroup;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Sound;
+import org.bukkit.block.TileState;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.EntityEquipment;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@Enchant(name = "Fortuna's Blessing", key = "fortunas_blessing", enchantChance = 0.45, maxLevel = 1,
+    targetGroup = TargetGroup.TILE_ENTITY, description = "While this banner is placed, there is a 45% " +
+    "chance that mobs killed within the same chunk will double their drops/experience and the probability " +
+    "of their equipment being dropped is increased by 50%.")
+
+public class FortunasBlessing extends BlockEnchant {
+
+  public FortunasBlessing(NamespacedKey key) {
+    super(key);
+  }
+
+  @Override
+  public boolean canTrigger(TileState tile) {
+    return hasEnchant(tile);
+  }
+
+  @Override
+  public boolean conflictsWith(Enchantment other) {
+    return false;
+  }
+
+  @EventHandler
+  public void onEntityDeath(EntityDeathEvent event) {
+
+    LivingEntity entity = event.getEntity();
+
+    // Gets nearby TileStates with this enchantment
+    Optional<TileState> tileState = Stream.of(entity.getLocation().getChunk().getTileEntities())
+        .filter(tileEntity -> tileEntity instanceof TileState)
+        .map(tileEntity -> (TileState) tileEntity)
+        .filter(this::canTrigger)
+        .findAny();
+
+    if (!tileState.isPresent()) return;
+
+    // Increase equipment drop chance
+    EntityEquipment equipment = entity.getEquipment();
+    if (equipment != null && !(entity instanceof Player)) {
+      increaseEquipmentDropChances(equipment);
+    }
+
+    // Drop every item again ecks dee
+
+    if (Math.random() < 0.45) {
+      entity.getWorld().playSound(entity.getLocation(), Sound.ENTITY_VILLAGER_CELEBRATE, 1F, 1F);
+      event.getDrops().forEach(item -> entity.getWorld().dropItemNaturally(entity.getLocation(), item));
+      event.setDroppedExp(event.getDroppedExp() * 2);
+    }
+
+  }
+
+  private void increaseEquipmentDropChances(EntityEquipment equipment) {
+
+    equipment.setItemInMainHandDropChance(getDropChance(equipment.getItemInMainHandDropChance()));
+    equipment.setItemInOffHandDropChance(getDropChance(equipment.getItemInOffHandDropChance()));
+    equipment.setHelmetDropChance(getDropChance(equipment.getHelmetDropChance()));
+    equipment.setChestplateDropChance(getDropChance(equipment.getHelmetDropChance()));
+    equipment.setLeggingsDropChance(getDropChance(equipment.getLeggingsDropChance()));
+    equipment.setBootsDropChance(getDropChance(equipment.getBootsDropChance()));
+
+  }
+
+  private float getDropChance(float original) {
+    return Math.min(1F, original + (original * 0.5F));
+  }
+
+}
