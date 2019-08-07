51334a1b79d989c5839a7fdd856e0c5176a44a1e
Jatyn Stacy <jlee0964@gmail.com>
2019-08-07 10:28:01 -0400

    HomingProjectile now aims for the target entity's center rather than their feet.

--- enchantments-core/src/main/java/net/jingles/enchantments/projectile/HomingProjectile.java
+++ enchantments-core/src/main/java/net/jingles/enchantments/projectile/HomingProjectile.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 public class HomingProjectile extends Projectile {
 
   private final Entity target;
+  private final double height;
   private double threshold = 25;
   private double accuracy = 0.25;
   private double localAccuracy = accuracy;
@@ -15,24 +16,27 @@ public class HomingProjectile extends Projectile {
   public HomingProjectile(Player owner, Entity target) {
     super(owner);
     this.target = target;
+    //Aiming for the entity's center
+    this.height = target.getHeight() / 2;
   }
 
   @Override
   public void move() {
 
-    if (target == null) {
+    if (target == null || target.isDead()) {
       Enchantments.getProjectileManager().unregister(this);
       return;
     }
 
+    Location location = target.getLocation().add(0, height, 0);
+
     double speed = getDirection().length();
     double addition;
-    double distance = getLocation().distanceSquared(target.getLocation());
+    double distance = getLocation().distanceSquared(location);
 
     addition = (distance > threshold) ? 0 : (threshold + 1) - distance / 5;
     localAccuracy += addition;
 
-    Location location = target.getLocation();
     setDirection(getDirection().add(location.subtract(getLocation()).toVector().normalize()
         .multiply(localAccuracy * speed)).normalize().multiply(speed));
     super.move();
